42fa91a22ca7ef8de6c4170bcc923ccbd4440515
jacob5555555555 <jacobeliasprinz@gmail.com>
2015-01-13 16:25:08 -0500

    ArrayPiece can do all but hashing

--- include/ArrayPiece.h
+++ include/ArrayPiece.h
@@ -1,6 +1,8 @@
 #ifndef ARRAYPIECE_H
 #define ARRAYPIECE_H
 
+#include <string.h>//memmove
+
 const static int EXTRA = 4;
 typedef char* DataPointer; //char* and not void* to avoid warnings
 
@@ -25,11 +27,25 @@ class ArrayPiece
             getArray()[getHead()->used] = val;
             ++getHead()->used;
         }
+        void replace(T val, int index){
+            getArray()[index] = val;
+        }
         T get(int index){
             return getArray()[index];
         }
         int size(){
             return getHead()->used;
+        }
+        void insert(T val, int index){
+            memmove(getArray()+index+1, getArray()+index, sizeof(T) * (size() - index));
+            replace(val, index);
+            ++getHead()->used;
+        }
+        ArrayPiece<T> subArray(int start, size_t size){
+            ArrayPiece<T> newArr(size);
+            memcpy(newArr.getArray(),getArray()+start, size * sizeof(T));
+            newArr.getHead()->used = size;
+            return newArr;
         }
         ~ArrayPiece() {
             free(data);
@@ -39,7 +55,7 @@ class ArrayPiece
             return (Head*) data;
         }
         T* getArray(){
-            return (T*)(data + sizeof(Head)); //cast to char does nothing but stops warning message
+            return (T*)(data + sizeof(Head));
         }
         struct Head{
             int used;
